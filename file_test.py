ab9533bc084bb339097327fa516781267788a6e7
Benoit <bdamay@gmail.com>
2024-02-18 14:29:52 +0000

    test action - correc PEP8 W293

--- file_test.py
+++ file_test.py
@@ -2,13 +2,13 @@ def test_calc_addition():
     # Fonction test du résultat de 2+4
     output = 2 + 4
     assert output == 6
- 
- 
+
+
 def test_calc_substraction():
     # Fonction test du résultat de 2-4
     output = 2 - 4
     assert output == -2
- 
+
 
 def test_calc_multiply():
     # Fonction test du résultat de 2*4
